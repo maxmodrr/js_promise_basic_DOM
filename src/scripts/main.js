04681388fff0b887fe3800867ac79c07fb27e1ad
maxmodrr <maxmodrr@gmail.com>
2024-10-23 01:50:29 +0300

    results

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,30 @@
 'use strict';
 
 // write your code here
+const h1 = document.querySelector('.logo');
+const body = document.body;
+
+const promSuccess = () => {
+  return new Promise((resolve) => {
+    const div = document.createElement('div');
+
+    div.classList.add('message');
+    div.textContent = 'Promise was resolved';
+    body.append(div);
+    resolve();
+  });
+};
+
+const promError = () => {
+  return new Promise((resolve, reject) => {
+    const div = document.createElement('div');
+
+    div.classList.add('message', 'error-message');
+    div.textContent = `Promise was rejected!`;
+    body.append(div);
+    reject(div);
+  });
+};
+
+h1.addEventListener('click', promSuccess);
+setTimeout(promError, 3000);
